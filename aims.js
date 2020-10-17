9838e19b91bff2b2ac6d9dfeb4ca93843df757b3
DrakeGoCoding <hoanglongphi21@gmail.com>
2020-10-17 21:00:11 +0700

    continue initialization

--- aims.js
+++ aims.js
@@ -1,17 +1,22 @@
-function displayRepository(){
+function displayRepository() {
     let repoZone = document.getElementsByClassName("repo-zone")[0];
-    if (repoZone.style.display === "none"){
+    let detailZone = document.getElementsByClassName("detail-zone")[0];
+    if (repoZone.style.display === "none") {
         repoZone.style.display = "block";
+        detailZone.style.width = "80vw";
+        detailZone.style.float = "right";
         return;
     }
     repoZone.style.display = "none";
+    detailZone.style.width = "auto";
+    detailZone.style.float = "none";
 }
 
-function createNewFolder(){
+function createNewFolder() {
 
 }
 
-function createNewFile(){
+function createNewFile() {
 
 }
 
